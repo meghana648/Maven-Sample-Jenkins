0429163f44e7f152efcb012b7f28940fd6a0f2d8
meghana648 <meghanagoud648@gmail.com>
2025-06-13 12:52:33 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,34 +1,21 @@
 package com.mycompany.app;
-import org.elasticsearch.common.vector.SimdVectorProcessor;
-//import org.elasticsearch.common.vector.Vector;
+
 
 /**
  * Hello world!
  */
-public class App {
+ public class App {
 
-   /* private static final String MESSAGE = "Hello World!";
-
+    private static final String MESSAGE = "Hello World!";
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        System.out.println(new App().getMessage());
     }
 
     public String getMessage() {
         return MESSAGE;
-    }*/
-    public static void main(String[] args) {
-        // Create a SimdVectorProcessor
-      /*   SimdVectorProcessor processor = new SimdVectorProcessor();
-
-        // Example vector operations
-        Vector vector1 = new Vector(new double[] {1.0, 2.0, 3.0});
-        Vector vector2 = new Vector(new double[] {4.0, 5.0, 6.0});
-
-        Vector result = processor.add(vector1, vector2); 
-
-        System.out.println("Result: " + result); // Output: Result: [5.0, 7.0, 9.0] */
-        System.out.println("Hello World ");
     }
+    
 }
+
